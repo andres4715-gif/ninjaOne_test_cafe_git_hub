2b9743325524d891e7d8b552a765b575ca62ff55
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-07-04 10:28:06 -0500

    Adding helpers and env variables

--- src/tests/exampleTest.ts
+++ src/tests/exampleTest.ts
@@ -1,13 +1,15 @@
+import "../globalSetup";
 import { Selector } from "testcafe";
 import loginPage from "../pages/examplePage";
+import { generateRandomString } from "../common/utilities/helpers";
 
 // env variables destructuring
-const { URL, USERNAME, PASSWORD } = process.env;
-
-fixture`Login Tests`.page(URL as string);
+const { USERNAME, PASSWORD } = process.env;
 
 test("User can log in with valid credentials", async (t) => {
   await loginPage.login(USERNAME as string, PASSWORD as string);
+  const randomString = generateRandomString(10);
+  console.info("--- The final random string is:", randomString);
 
   const loggedInUser = Selector("#flash-messages");
   await t.expect(loggedInUser.exists).ok();

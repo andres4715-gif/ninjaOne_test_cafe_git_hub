c26dc20719a7c1ef12d6b8eaf1ae1a03a8231b2f
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-07-05 06:57:31 -0500

    github action v5

--- src/tests/loginTest.ts
+++ src/tests/loginTest.ts
@@ -9,7 +9,7 @@ const { USERNAME, PASSWORD } = process.env;
 test("User can log in with valid credentials", async (t) => {
   await loginPage.login(USERNAME as string, PASSWORD as string);
   const randomString = generateRandomString(10);
-  console.info("--- The final random string is:", randomString);
+  console.info("--- The final random string:", randomString);
 
   const loggedInUser = Selector("#flash-messages");
   await t.expect(loggedInUser.exists).ok();

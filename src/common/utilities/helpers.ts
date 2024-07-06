711137406615024b2436fe6e8fa091ff5818aff6
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-07-06 08:20:28 -0500

    Adding helpers to test

--- src/common/utilities/helpers.ts
+++ src/common/utilities/helpers.ts
@@ -1,5 +1,32 @@
+import logger from "../../utils/logger";
+
+/**
+ * Generates a random string of the specified length.
+ *
+ * This function uses `Math.random()` to generate a random number,
+ * converts it to a base-36 string (which includes letters and numbers),
+ * and then extracts a substring of the specified length.
+ *
+ * @param {number} length - The desired length of the random string.
+ * @returns {string} A random string of the specified length.
+ */
 export const generateRandomString = (length: number): string => {
   const newRandomString = Math.random().toString(36).substring(2, length);
   console.info("--- The obtained random string is:", newRandomString);
   return newRandomString;
+};
+
+/**
+ * Logs the provided API response data as a JSON string.
+ *
+ * This function takes an object, converts it to a pretty-printed JSON string,
+ * and logs it using the `logger.info` method.
+ *
+ * @param {object} apiResponseData - The API response data to be logged.
+ * @returns {object} The result of the logger.info method call.
+ */
+export const logInfoJsonStringify = (apiResponseData: object): object => {
+  return logger.info(
+    `--- Devices: ${JSON.stringify(apiResponseData, null, 2)}`,
+  );
 };
--- src/tests/test1.ts
+++ src/tests/test1.ts
@@ -1,5 +1,5 @@
 import "../utils/setup";
-import logger from "../utils/logger";
+import { logInfoJsonStringify } from "../common/utilities/helpers";
 import axios from "axios";
 
 // env variables destructuring
@@ -16,6 +16,6 @@ fixture`Test 1`.page(url);
 test("Make an API call to retrieve the list of devices", async (tc) => {
   const response = await axios.get(`${baseUrl}${endpoints.devices}`);
   await tc.expect(response.status).eql(200, "--- API call failed");
-  const devices = response.data;
-  logger.info(`--- List of Devices: ${JSON.stringify(devices, null, 2)}`);
+  const responseDevices = response.data;
+  logInfoJsonStringify(responseDevices);
 });

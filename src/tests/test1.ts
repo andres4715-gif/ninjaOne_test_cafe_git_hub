cad3697419e189f4d9ca8f6c33186cae896f072c
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-07-06 07:26:06 -0500

    new readme and getting the base url form .env

--- src/tests/test1.ts
+++ src/tests/test1.ts
@@ -3,19 +3,19 @@ import logger from "../utils/logger";
 import axios from "axios";
 
 // env variables destructuring
-const { URL } = process.env;
+const { URL, BASEURL } = process.env;
 
-const dataDevices = {
-  baseUrl: "http://localhost:3000",
-  endpoint: "devices",
+const url = URL as string;
+const baseUrl = BASEURL as string;
+
+const endpoints = {
+  devices: "devices",
 };
 
-fixture`Test 1`.page(URL as string);
+fixture`Test 1`.page(url);
 test("Make an API call to retrieve the list of devices", async (tc) => {
-  const response = await axios.get(
-    `${dataDevices.baseUrl}/${dataDevices.endpoint}`,
-  );
+  const response = await axios.get(`${baseUrl}${endpoints.devices}`);
   await tc.expect(response.status).eql(200, "--- API call failed");
   const devices = response.data;
-  logger.info(`--- Devices: ${JSON.stringify(devices, null, 2)}`);
+  logger.info(`--- List of Devices: ${JSON.stringify(devices, null, 2)}`);
 });

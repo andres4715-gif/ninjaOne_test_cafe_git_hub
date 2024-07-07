9ec70f04bd70d48f00985eae36a6f768057f46a2
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-07-07 12:47:36 -0500

    data form UI READY

--- src/tests/test1.ts
+++ src/tests/test1.ts
@@ -2,6 +2,9 @@ import "../utils/setup";
 import { logInfoJsonStringify } from "../common/utilities/helpers";
 import { verifyTypeOfArray } from "../common/utilities/assertions";
 import axios from "axios";
+// import logger from "../utils/logger";
+import { Device } from "../types/deviceTypes";
+import homeSelectors from "../selectors/homeSelectors";
 import logger from "../utils/logger";
 
 // env variables destructuring
@@ -16,16 +19,36 @@ const endpoints = {
 
 fixture`Test 1`.page(url);
 test("Make an API call to retrieve the list of devices", async (t) => {
-  const response = await axios.get(`${baseUrl}${endpoints.devices}`);
+  const response = await axios.get<Device[]>(`${baseUrl}${endpoints.devices}`);
   await t.expect(response.status).eql(200, "--- API call failed");
-  const responseDevices = response.data;
+  const responseDevices: Device[] = response.data;
   logInfoJsonStringify(endpoints.devices, responseDevices);
   verifyTypeOfArray(t, endpoints.devices, responseDevices);
 
-  logger.info("--- Interac with the object");
-  console.log("****** 0", responseDevices[0]);
-  console.log("****** 1", responseDevices[1]);
-  console.log("****** 2", responseDevices[2]);
-});
+  const deviceNameUi: Selector = homeSelectors.device_name;
 
-// todo check the return to get values of variables.
+  // Ensure the selector is used with the test controller
+  logger.info(`--- Device info obtained from UI`);
+  const deviceInfos = await deviceNameUi
+    .with({ boundTestRun: t })
+    .count.then((count) =>
+      Promise.all(
+        Array.from({ length: count }).map(async (_, i) => ({
+          name: await deviceNameUi
+            .nth(i)
+            .find(".device-name")
+            .with({ boundTestRun: t }).innerText,
+          type: await deviceNameUi
+            .nth(i)
+            .find(".device-type")
+            .with({ boundTestRun: t }).innerText,
+          capacity: await deviceNameUi
+            .nth(i)
+            .find(".device-capacity")
+            .with({ boundTestRun: t }).innerText,
+        })),
+      ),
+    );
+
+  console.log("--- Device Infos: ", deviceInfos);
+});
